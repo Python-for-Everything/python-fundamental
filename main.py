1d624eaf86eadfe5a41505079feb763291605095
riki-graha <riki.graha@gmail.com>
2024-08-13 14:13:49 +0700

    comment for README.md

--- main.py
+++ main.py
@@ -11,5 +11,19 @@ print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu>0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+
+
+
+
+
 
 
