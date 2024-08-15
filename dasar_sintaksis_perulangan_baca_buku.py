509a89894738c6c06a3e92ab604177e051bf7f85
riki-graha <riki.graha@gmail.com>
2024-08-15 14:11:24 +0700

    Branching with for & without for.md

---
+++ dasar_sintaksis_perulangan_baca_buku.py
@@ -0,0 +1,32 @@
+"""
+Program Perulangan Membaca Buku
+"""
+
+jumlah_buku = 100
+print('Ibu Berkata, "Baca Semua Bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+print("Dengan for")
+for jumlah_buku_yang_sudah_dibaca in range  (1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f'Jumlah_buku_yang_sudah_dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+# tanpa for
+print("Tanpa for")
+print("Membaca buku ke-1")
+print("Membaca buku ke-2")
+print("Membaca buku ke-3")
+print("Membaca buku ke-4")
+print("Membaca buku ke-5")
+print("Membaca buku ke-6")
+print("Membaca buku ke-7")
+print("Membaca buku ke-8")
+print("Membaca buku ke-9")
+print("Membaca buku ke-10")
+print("Membaca buku ke-11")
+print("Membaca buku ke-12")
+print("Membaca buku ke-13")
+print("Membaca buku ke-14")
