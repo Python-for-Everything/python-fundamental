31c7ab320cebcdaa4ea9427c8a83acfe746fa275
riki-graha <riki.graha@gmail.com>
2024-09-02 11:11:10 +0700

    change variable to English

--- dasar_sintaksis_perulangan_baca_buku_dengan_while.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while.py
@@ -1,16 +1,17 @@
 """
-Program Perulangan Membca Buku dengan While
+Program Perulangan Membaca Buku dengan While
 """
 jumlah_buku = 1000000
-print('Ibu Berkata, "Baca Semua Bukumu"')
+print('Ibu berkata, "Baca semua bukumu"')
 
 jumlah_buku_yang_sudah_dibaca = 0
 print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
-print("Dengan While")
 while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
     jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
 
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+
 
---
+++ dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,34 @@
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+print('Tampilkan variable daftar buku')
+print(daftar_buku)
+
+print('\nProses semua dengan for in')
+for buku in daftar_buku:
+    print(buku)
+
+print('\nTampilkan isi daftar_buku pada indexs tertentu ')
+print(daftar_buku[0])
+print(daftar_buku[2])
+
+print('\n Tampilkan dengan for i range')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku = [1, 'Kenji Volume 2', -313, 3.14]
+print('\nTampilkan dengan for in range, dimana tipe data tiap elemen berbeda beda')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nKembalikan nilai awal daftar buku')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+print('\nTambahkan 1 buku baru')
+daftar_buku.append('Dunia Matematika Kelas 5')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+
+
+
+
+
