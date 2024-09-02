a499c6f6ba72e591576c52af83467501cfd24218
riki-graha <riki.graha@gmail.com>
2024-09-02 11:32:05 +0700

    Use List

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -26,9 +26,47 @@ daftar_buku.append('Dunia Matematika Kelas 5')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\nClear List')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print('\nGanti Element Pertama')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print('\nBuku yang diambil tadi')
+print(buku)
+daftar_buku.pop()
 
+print('\nPop -1')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+daftar_buku.pop(-1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
+print('\nPop -2')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+daftar_buku.pop(-2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop -3')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+daftar_buku.pop(-3)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop -4')
+daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
+daftar_buku.pop(-4)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
