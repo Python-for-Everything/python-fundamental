c598db38b6bb32d96e127c107ff40a2db814b21b
riki-graha <riki.graha@gmail.com>
2024-09-03 13:24:32 +0700

    dasar sintaksis tipe data list

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -94,20 +94,3 @@ del daftar_buku[3]
 for i in range(3, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\nPerintah Del dengan list comprehension')
-daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
-del daftar_buku[:]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah Del dengan list comprehension start // tidak ada yang dihapus')
-daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
-del daftar_buku[0:0] #START:END
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah Del dengan list comprehension start stop with step')
-daftar_buku = ['Seven Habits','How to Influence People','First Things Frist','4DX']
-del daftar_buku[0::2] #START:END:STEP
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
