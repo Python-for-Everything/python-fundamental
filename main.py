c32191c7b7e56bc48de481fbe708ed00673f2f57
Riki Satya Graha <riki.graha@gmail.com>
2024-09-30 11:09:41 +0700

    add command for # percabangan.

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari :
 # Sekuensial
 print('Ibu Berkata, "Pergi ke Toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?')
-print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print('Ibu menjawab, "Beli satu botol susu"')
 print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu >0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("menyampaikan hasilnya kepada Ibu")
